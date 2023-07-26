e5ff63a5fadfb345f987132f9a3873eb0815d131
Thomas Nikoloff <tomnikoloff@hotmail.co.uk>
2023-07-26 15:22:51 +0100

    working game

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -6,9 +6,11 @@ import { Card } from "../Card/Card";
 import { Results } from "../Results/Results";
 
 const shuffleCards = (array) => {
+
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = array[i];
+      
       array[i] = array[j];
       array[j] = temp;
     }
@@ -22,9 +24,19 @@ export default function Board(){
         () => shuffleCards(ImagesArr)
     );
 
-    const handleCardClick = (index) => {
-        console.log('test click', index);
+    const [userChoice, setUserChoice] = useState(null);
+    const [computerChoice, setComputerChoice] = useState(null);
+    const [result, setResult] = useState(null);
+    const [userCard, setUserCard] = useState(null);
+    const [computerCard, setComputerCard] = useState(null)
 
+    const handleCardClick = (choice) => {
+        
+        setUserChoice(choice);
+
+        generateComputerChoice();
+
+        
         let startGrid = document.getElementById('start_grid');
         console.log(startGrid);
         startGrid.classList.add('uk-hidden');
@@ -32,12 +44,52 @@ export default function Board(){
         let resultsGrid = document.getElementById('results_grid');
         console.log(resultsGrid);
         resultsGrid.classList.remove('uk-hidden');
+        
 
     }
 
-    const [botCards, setBotCards] = useState(
-            () => ImagesArr[Math.floor(Math.random()*ImagesArr.length)]
-    );
+    const choices = ['rock', 'paper', 'scissors'];
+
+    const generateComputerChoice = () => {
+
+        const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+        setComputerChoice(randomChoice);
+
+    }
+
+    const evaluate = () => {
+
+        switch( userChoice + computerChoice) {
+            case 'scissorspaper':
+            case 'rockscissors':
+            case 'paperrock':
+                setResult('You Win')
+                break
+            case 'paperscissors':
+            case 'scissorsrock':
+            case 'rockpaper':
+                setResult('You Lose')
+                break
+            case 'rockrock':
+            case 'paperpaper':
+            case 'scissorsscissors':
+                setResult('You Draw')
+                break
+        }
+
+        cards.map((card, index) => {
+            card.type == userChoice ? setUserCard(card.image) : '';
+            card.type == computerChoice ? setComputerCard(card.image) : '';
+        })
+
+    }
+
+    useEffect(() => {
+
+        evaluate();
+        
+    }, [userChoice, computerChoice])
+
 
     return (
         <>
@@ -54,13 +106,27 @@ export default function Board(){
                                 card={card}
                                 index={index}
                                 
-                                onClick={handleCardClick}
+                                onClick={() => handleCardClick(card.type)}
                             />
                         )
                     })}
                 </div>  
                 <div id="results_grid" className="uk-grid uk-child-width-1-3 uk-hidden">
-
+                    <div className="uk-flex uk-flex-center uk-flex-middle uk-animation-slide-left">
+                        <div className="card">
+                            <img  className="card-img" src={userCard} />
+                        </div>
+                    </div>
+                    <div className="uk-flex uk-flex-center uk-flex-middle">
+                        <div>
+                            <button>Play Again</button>
+                        </div>
+                    </div>
+                    <div className="uk-flex uk-flex-center uk-flex-middle uk-animation-slide-right">
+                        <div className="card">
+                            <img  className="card-img" src={computerCard} />
+                        </div>
+                    </div>
                 </div>  
             </div>
         </>
